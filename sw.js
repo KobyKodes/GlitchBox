dbfceba3bb59d1f9d8682b594018042b9e27ae21
jad-ko <jadkobrosly@gmail.com>
2026-02-07 13:02:00 +0200

    changed app icon to a new visual

--- sw.js
+++ sw.js
@@ -1,3 +1,39 @@
-// v3
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
+// v4
+const CACHE_NAME = 'glitchbox-v1';
+const PRECACHE = [
+  '/',
+  '/index.html',
+  '/Icons/GlitchBoxFavicon.png',
+  '/Icons/GlitchBoxGlassCube.PNG'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(PRECACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then((response) => {
+        // Cache successful GET responses for faster future loads
+        if (e.request.method === 'GET' && response.ok) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(e.request, clone));
+        }
+        return response;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
